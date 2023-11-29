55f75ae498c32b9f97c7b60fd4480a8ad2da2b8b
OlaKrzemien <101366662+OlaKrzemien@users.noreply.github.com>
2023-11-29 22:11:20 +0300

    Add files via upload

---
+++ custom-php-framework/config/config.php
@@ -0,0 +1,6 @@
+<?php
+$config = [];
+$config['db_dsn'] = 'sqlite:' . __DIR__ . '/../data.db';
+//$config['db_dsn'] = 'sqlite:C:\Users\Ola\AppData\Roaming\JetBrains\PhpStorm2023.2\consoles\db\data.db';
+$config['db_user'] = '';
+$config['db_pass'] = '';
---
+++ custom-php-framework/src/Model/Comment.php
@@ -0,0 +1,140 @@
+<?php
+namespace App\Model;
+
+use App\Service\Config;
+
+class Comment
+{
+    private ?int $id = null;
+    private ?string $subject = null;
+    private ?string $content = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(?int $id): Comment
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getSubject(): ?string
+    {
+        return $this->subject;
+    }
+
+    public function setSubject(?string $subject): Comment
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(?string $content): Comment
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public static function fromArray($array): Comment
+    {
+        $com = new self();
+        $com->fill($array);
+
+        return $com;
+    }
+
+    public function fill($array): Comment
+    {
+        if (isset($array['id']) && ! $this->getId()) {
+            $this->setId($array['id']);
+        }
+        if (isset($array['subject'])) {
+            $this->setSubject($array['subject']);
+        }
+        if (isset($array['content'])) {
+            $this->setContent($array['content']);
+        }
+
+        return $this;
+    }
+
+    public static function findAll(): array
+    {
+        $pdo = new \PDO(Config::get('db_dsn'), Config::get('db_user'), Config::get('db_pass'));
+        $sql = 'SELECT * FROM comment';
+        $statement = $pdo->prepare($sql);
+        $statement->execute();
+
+        $coms = [];
+        $comsArray = $statement->fetchAll(\PDO::FETCH_ASSOC);
+        foreach ($comsArray as $comArray) {
+            $coms[] = self::fromArray($comArray);
+        }
+
+        return $coms;
+    }
+
+    public static function find($id): ?Comment
+    {
+        $pdo = new \PDO(Config::get('db_dsn'), Config::get('db_user'), Config::get('db_pass'));
+        $sql = 'SELECT * FROM comment WHERE id = :id';
+        $statement = $pdo->prepare($sql);
+        $statement->execute(['id' => $id]);
+
+        $comArray = $statement->fetch(\PDO::FETCH_ASSOC);
+        if (! $comArray) {
+            return null;
+        }
+        $com = Comment::fromArray($comArray);
+
+        return $com;
+    }
+
+    public function save(): void
+    {
+        $pdo = new \PDO(Config::get('db_dsn'), Config::get('db_user'), Config::get('db_pass'));
+        if (! $this->getId()) {
+            $sql = "INSERT INTO comment (subject, content) VALUES (:subject, :content)";
+            $statement = $pdo->prepare($sql);
+            $statement->execute([
+                'subject' => $this->getSubject(),
+                'content' => $this->getContent(),
+            ]);
+
+            $this->setId($pdo->lastInsertId());
+        } else {
+            $sql = "UPDATE comment SET subject = :subject, content = :content WHERE id = :id";
+            $statement = $pdo->prepare($sql);
+            $statement->execute([
+                ':subject' => $this->getSubject(),
+                ':content' => $this->getContent(),
+                ':id' => $this->getId(),
+            ]);
+        }
+    }
+
+    public function delete(): void
+    {
+        $pdo = new \PDO(Config::get('db_dsn'), Config::get('db_user'), Config::get('db_pass'));
+        $sql = "DELETE FROM comment WHERE id = :id";
+        $statement = $pdo->prepare($sql);
+        $statement->execute([
+            ':id' => $this->getId(),
+        ]);
+
+        $this->setId(null);
+        $this->setSubject(null);
+        $this->setContent(null);
+    }
+}
+
